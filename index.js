590b930b8626086383b0512b3aec560c111fc3c7
Thoyt810 <66744198+Thoyt810@users.noreply.github.com>
2020-07-21 15:33:02 -0600

    debugging cant figure out the read me script"
    "

--- index.js
+++ index.js
@@ -2,50 +2,112 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
 
-inquirer.promptUser([
-    {
-      type: "input",
-      message: "What is your Github Username?",
-      name: "username"
-    },
-    {
-      type: "input",
-      message: "What is your email address?",
-      name: "github"
-    },
-    {
-      type: "input",
-      message: "What is your project name?",
-      name: "projectName"
-    },
-    {
-        type: "input",
-        message: "Please write a short description of your project.",
-        name: "projectName"
-      },
-      {
-        type: "input", //change to multiple choice
-        message: "What kind of license should your project have?",
-        name: "license"
-      },
-      {
-        type: "input", // default answer 
-        message: "What command should be run for to install dependencies?",
-        name: "dependencies"
-      },
-      {
-        type: "input", // default answer
-        message: "What command should be run to run tests?",
-        name: "tests"
-      },
-      {
-        type: "input",
-        message: "What does the user need to know about this Repository?",
-        name: "userInfo"
-      },
-      {
-        type: "input",
-        message: "What does the user need to know about contributing to this Repository?",
-        name: "userInput"
-      }
-  ])
+const writeFile = util.promisify(fs.writeFile);
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your Github Username?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "github"
+        },
+        {
+            type: "input",
+            message: "What is your project name?",
+            name: "projectName"
+        },
+        {
+            type: "input",
+            message: "Please write a short description of your project.",
+            name: "projectDescription"
+        },
+        {
+            type: "checkbox",
+            message: "What kind of license should your project have?",
+            name: "license",
+            choices: [
+                "MIT",
+                "APache 2.0",
+                "GPL 3.0",
+                "BSD 3",
+                "None",
+            ]
+        },
+        {
+            type: "checkbox",
+            message: "What command should be run for to install dependencies?",
+            name: "dependencies",
+            choices: [
+                "npm i"
+            ]
+        },
+        {
+            type: "checkbox",
+            message: "What command should be run to run tests?",
+            name: "tests",
+            choices: [
+                "npm test"
+            ]
+        },
+        {
+            type: "input",
+            message: "What does the user need to know about this Repository?",
+            name: "userInfo"
+        },
+        {
+            type: "input",
+            message: "What does the user need to know about CONTRIBUTING to this Repository?",
+            name: "userInput"
+        }
+    ])
+}
+
+function makeReadMe(answers) {
+`
+Github Username
+
+${answers.username}
+
+##Github Email Adress
+
+${answers.github}
+
+## Project Name
+
+${answers.projectName}
+
+## Project Description
+
+${answers.projectDescription}
+
+## Table of Contents
+
+${answers.license}
+
+${answers.dependencies}
+
+${answers.tests}
+
+## User Info
+
+What does the user need to know about the Repository?
+
+${answers.userInfo}
+
+What does the User need to know about CONTRIBUTING to the Repository?
+
+${answers.userInput}
+
+`
+}
+
+promptUser()
+    .then(function(answers) {
+        const readMe = makeReadMe(answers);
+        return writeFile("READ.Me", readMe )
+    })
