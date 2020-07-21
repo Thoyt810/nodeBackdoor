79837ded8b097d5013d5538f5e9b5469b7d64b5d
Thoyt810 <66744198+Thoyt810@users.noreply.github.com>
2020-07-21 09:44:02 -0600

    forogot to push this last night still work to do

--- index.js
+++ index.js
@@ -4,17 +4,47 @@ const inquirer = require("inquirer");
 inquirer.prompt([
     {
       type: "input",
-      message: "",
+      message: "What is your Github Username?",
       name: "username"
     },
     {
       type: "input",
-      message: "",
+      message: "What is your email address?",
       name: "github"
     },
     {
       type: "input",
-      message: "",
-      name: "confirm"
-    }
+      message: "What is your project name?",
+      name: "projectName"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project.",
+        name: "projectName"
+      },
+      {
+        type: "input", //change to multiple choice
+        message: "What kind of license should your project have?",
+        name: "license"
+      },
+      {
+        type: "input", //change
+        message: "What command should be run for to install dependencies?",
+        name: "dependencies"
+      },
+      {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests"
+      },
+      {
+        type: "input",
+        message: "What does the user need to know about this Repository?",
+        name: "userInfo"
+      },
+      {
+        type: "input",
+        message: "What does the user need to know about contributing to this Repository?",
+        name: "userInput"
+      }
   ])
